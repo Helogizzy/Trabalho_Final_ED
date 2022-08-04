398025846385c12e322524f5a7719c09c8b0cd67
Gabriel Mazzuco <gabrielalvesmazzuco@hotmail.com>
2022-08-04 11:28:18 -0300

    Add files via upload

--- HeapSort/main.c
+++ HeapSort/main.c
@@ -8,12 +8,16 @@
     do ultimo nível, todos os elementos 'pai' do vetor, possuem dois elementos
     como 'filhos'*/
 
-int main(int argc, char *argv[]){
+int main(void){
 
     /*Pegamos o valor do tamanho do vetor por argumentação, que é passada
       dentro do makefile do programa*/
-    int TAM = strtol(argv[1], NULL, 10);
+    int TAM;
     int vet[TAM];
+
+    //Determina quanto será o tamanho do vetor
+    printf("Determine o tamanho do vetor: ");
+    scanf("%d",&TAM);
 
     //Randomiza o vetor com números aleatorios
     randomize_vet(vet, TAM);
